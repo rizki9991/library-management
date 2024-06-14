080f0aa0111b82ea4f8149668fdf4df2db191137
muhammad rizki <muhamadrizki9991@gmail.com>
2024-06-14 10:06:51 +0700

    app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MembersModule } from './member/members.module';
+import { BooksModule } from './books/books.module';
+import { Member } from './member/member.entity';
+import { Book } from './books/book.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql', // atau 'postgres' jika menggunakan PostgreSQL
+            host: 'localhost',
+            port: 3306, // atau 5432 jika menggunakan PostgreSQL
+            username: 'root', // sesuaikan dengan username database Anda
+            password: '', // sesuaikan dengan password database Anda
+            database: 'library', // nama database yang Anda buat
+            entities: [Member, Book],
+            synchronize: true,
+        }),
+        MembersModule,
+        BooksModule,
+    ],
 })
-export class AppModule {}
+export class AppModule { }
