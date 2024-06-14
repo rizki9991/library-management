e07963f25cce7f97b3eecd8946729f24dc53b56d
muhammad rizki <muhamadrizki9991@gmail.com>
2024-06-14 10:07:00 +0700

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    // Konfigurasi Swagger
+    const config = new DocumentBuilder()
+        .setTitle('Library API')
+        .setDescription('API documentation for the Library application')
+        .setVersion('1.0')
+        .addTag('library')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
